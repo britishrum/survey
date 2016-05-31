31945001d8847a4f4bbea954e2c2e166a8fb5ee6
artyom2009a <artyom2009a@gmail.com>
2016-05-31 11:19:51 +0300

    Close the task CreateJSONWithQuestions.

---
+++ client/questions.js
@@ -0,0 +1,20 @@
+[
+{
+"id" : "01",	
+"type" : "multiple",
+"eng" : {"question" : "How are you?", "answers" : ["Good", "Bad"]},
+"rus" : {"question" : "Как дела?", "answers" : ["Хорошо", "Плохо"]} 
+},
+{
+"id" : "02",	
+"type" : "free",
+"eng" : {"question" : "How old are you?"},
+"rus" : {"question" : "Сколько тебе лет?"}
+},
+{
+"id" : "03",	
+"type" : "range",
+"eng" : {"question" : "How are you today?"},
+"rus" : {"question" : "Какое у тебя сегодня настроение?"}
+}
+]
