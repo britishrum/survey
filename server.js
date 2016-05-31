470377c0b9e46da639102994efe9bd782eac0ca2
Aidar Shaikhiev <muphblu09@gmail.com>
2016-05-31 11:26:35 +0300

    Initial commit

--- server.js
+++ server.js
@@ -3,14 +3,23 @@ var app = express();
 
 app.use('/static', express.static(__dirname + '/client'));
 
-app.get('/survey', function(req, res) {
-
-});
-
 app.use('/', function(req, res) {
     res.sendFile(__dirname + '/client/index.html');
 });
 
 app.listen(81, function() {
     console.log('Survey app listening on port 81');
+});
+
+//survey
+app.get('/survey', function(req, res) {
+	res.sendFile(__dirname + '/client/survey.html');
+});
+
+app.get('/questions', function(req, res) {
+	res.json(/*Place for json from Artem*/);
+});
+
+app.post('/submit', function(req, res){
+	res.sendFile(__dirname + '/client/survey.html');
 });
