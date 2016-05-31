20e8779abb80fda5ac65d05e4be3f07eb6f6f394
Aidar Shaikhiev <muphblu09@gmail.com>
2016-05-31 11:38:06 +0300

    stub for Artem questions

---
+++ questions.js
@@ -0,0 +1,8 @@
+module.exports = {
+  foo: function () {
+  	return {data: "test"};
+  },
+  bar: function () {
+    // whatever
+  }
+};
--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 var express = require('express');
+var questions = require('./questions.js');
 var app = express();
 
 app.use('/static', express.static(__dirname + '/client'));
+
+
+//survey
+app.get('/survey', function(req, res) {
+	res.sendFile(__dirname + '/client/survey.html');
+});
+
+app.get('/questions', function(req, res) {
+	res.json(questions.foo());
+});
+
+app.post('/submit', function(req, res){
+	res.sendFile(__dirname + '/client/survey.html');
+});
+
+
 
 app.use('/', function(req, res) {
     res.sendFile(__dirname + '/client/index.html');
@@ -11,15 +28,3 @@ app.listen(81, function() {
     console.log('Survey app listening on port 81');
 });
 
-//survey
-app.get('/survey', function(req, res) {
-	res.sendFile(__dirname + '/client/survey.html');
-});
-
-app.get('/questions', function(req, res) {
-	res.json(/*Place for json from Artem*/);
-});
-
-app.post('/submit', function(req, res){
-	res.sendFile(__dirname + '/client/survey.html');
-});
