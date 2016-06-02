f85c8bab9556b3e703a13807047ff1333a484e07
Sasha Faz <britishrum@gmail.com>
2016-06-02 23:27:50 +0300

    -

--- server.js
+++ server.js
@@ -5,9 +5,18 @@ var databaseUrl = "surveyDb";
 var collections = ["questions", "answers", "users"];
 var mongo = require("mongojs")(databaseUrl, collections);
 
+var passport = require('passport');
+var basicStrategy = require('passport-http');
+
+console.log(basicStrategy);
+
 app.use('/static', express.static(__dirname + '/client'));
 
 app.get('/survey', function(req, res) {
+
+});
+
+app.get('/login', function(req, res, next) {
 
 });
 
