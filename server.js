494cecf34db306005ad820143e194e50561e74a6
Aidar Shaikhiev <muphblu09@gmail.com>
2016-06-05 23:33:06 +0300

    test withiout mongo

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var env = process.env;
 
 var databaseUrl = "admin:AeE7PhSbhem2@localhost/surveyapp";
 var collections = ["questions", "answers", "users"];
-var mongo = require("mongojs")(databaseUrl, collections);
+//var mongo = require("mongojs")(databaseUrl, collections);
 var bodyParser = require('body-parser');
 
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 var passport = require('passport');
 var BasicStrategy = require('passport-http').BasicStrategy;
 
-require('./db.js')(mongo);
+/*require('./db.js')(mongo);
 
 passport.use(new BasicStrategy(function(userid, password, done) {
     mongo.users.find({
@@ -26,7 +26,7 @@ passport.use(new BasicStrategy(function(userid, password, done) {
         if (!users.length) { return done(null, false); }
         return done(null, users[0]);
     });
-}));
+}));*/
 
 app.use('/static', express.static(__dirname + '/client'));
 
@@ -74,5 +74,5 @@ app.use('/', function(req, res) {
 });
 
 app.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function() {
-    console.log('Survey app listening on port 80');
+    console.log('Survey app listening on port '+ env.NODE_PORT || 3000);
 });
